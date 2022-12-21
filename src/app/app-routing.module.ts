9275676e059094638b87baee0bf943aff8af5e79
Yocmar Jiménez <yocmarjigon@gmail.com>
2022-12-21 10:57:51 -0500

    generado el nav de la pagina web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', loadChildren: ()=> import('./module/inicio/inicio.module').then(m => m.InicioModule)},
+  {path: '', redirectTo: '/inicio', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
