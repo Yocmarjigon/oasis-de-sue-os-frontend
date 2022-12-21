0f8b4ee1c51bef33a68486f77e52d9d90d1cf540
BrayanGuzman01 <111928338+BrayanGuzman01@users.noreply.github.com>
2022-12-21 13:49:01 -0500

    Primer commit de Brayan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './module/inicio/index/index.component';
 
 const routes: Routes = [
-  {path: 'inicio', loadChildren: ()=> import('./module/inicio/inicio.module').then(m => m.InicioModule)},
-  {path: '', redirectTo: '/inicio', pathMatch:'full'}
+  {path: 'inicio', component: IndexComponent},
+  {path: '', redirectTo: '/inicio', pathMatch:'full'},
+  {path: 'nosotros', loadChildren: ()=> import('./module/nosotros/nosotros.module').then(m=>m.NosotrosModule)}
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
