8a9b868cb084712c7dd09ebd3b563e0ca9bef08d
Yocmar Jiménez <yocmarjigon@gmail.com>
2022-12-21 13:43:25 -0500

    primer commit de la rama yocmar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './module/inicio/index/index.component';
 
 const routes: Routes = [
-  {path: 'inicio', loadChildren: ()=> import('./module/inicio/inicio.module').then(m => m.InicioModule)},
+  {path: 'inicio', component: IndexComponent},
   {path: '', redirectTo: '/inicio', pathMatch:'full'}
 ];
 
--- src/app/plantilla/nav-bar/nav-bar.component.ts
+++ src/app/plantilla/nav-bar/nav-bar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
+
+
+
 export class NavBarComponent {
+  @ViewChild('asBtnNav') btnNab!: ElementRef;
+
+  constructor( private route: Router ,private render2: Renderer2){}
+
+  activador (){
+    const btnNab = this.btnNab.nativeElement; 
+    this.render2.setStyle(btnNab, 'border-botton', 'solid 2px #1AA94F')
+  }
 
 }
