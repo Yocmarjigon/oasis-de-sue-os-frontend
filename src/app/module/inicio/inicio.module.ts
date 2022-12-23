72d0a91ea6fc73cbb626eef04162e9a92e2c915b
BrayanGuzman01 <111928338+BrayanGuzman01@users.noreply.github.com>
2022-12-23 12:04:39 -0500

    Cambios en el header

--- src/app/module/inicio/inicio.module.ts
+++ src/app/module/inicio/inicio.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index/index.component';
+import { UneteCorporacionComponent } from './unete-corporacion/unete-corporacion.component';
 
 
 
 @NgModule({
   declarations: [
-    IndexComponent
+    IndexComponent, 
+    UneteCorporacionComponent
   ],
   imports: [
     CommonModule
